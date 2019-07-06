3cbc331b4ba3d277359fdcd3e4e19e014d75b32e
VillardX <43296123+VillardX@users.noreply.github.com>
2019-07-06 23:42:14 +0800

    Update crawler_for_allgame.py

--- web_crawler/crawler_for_allgame.py
+++ web_crawler/crawler_for_allgame.py
@@ -4,18 +4,26 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import time 
 
-#按排名爬全部游戏
-basement = r'https://bangumi.tv'#每款游戏的基础网址
-start_page = r'https://bangumi.tv/game/browser'#每一页的基础网址网址
-present_page = r'https://bangumi.tv/game/browser?sort=rank&page=1'#第一页
+basement = r'https://bangumi.tv'#单款产品基础网址
+start_page = r'http://bangumi.tv/game/browser'#单页面基础网址
+present_page = r'http://bangumi.tv/game/browser?page=1'#当前地址
 
+#数据结构
 raw_data = pd.DataFrame(columns=['id','total','characters','persons','comments'])#存储数据的dataframe
 
-#爬该页的所有信息
+#累计爬取产品数
+n = 0
 
-n = 0#当前所爬产品数
+#在爬取过的信息基础上爬新的信息，去重
+already_item_path = r'D:\pycode\ACGN\raw_data_already.xlsx'
+already_item_data = pd.read_excel(already_item_path)
+already_item_id = already_item_data['id']
+already_item_id = list(already_item_id)
 
+
+#爬取该页所有信息
 while True:
     print('正在爬' + present_page)
     present_html = urlopen(present_page)
@@ -23,19 +31,23 @@ while True:
     #先抓取该页上的所有游戏信息
     items_list = present_bs0bj.find('ul',{'id':"browserItemList",'class':"browserFull"}).findAll('a',{'class':'subjectCover cover ll'})#包含了该页面的所有游戏
     for each_item in items_list:
-        browse_page = basement + each_item['href']
-        temp = sif.one_item(browse_page)
-        if temp.is_crawlable():
-            temp.info_total()
-            temp.info_characters()
-            temp.info_comments()
-            temp.info_persons()
-            #print(temp.info)
-            raw_data.loc[raw_data.shape[0]] = temp.info#添加一行，即新爬到的产品的信息
+        fi_id = int(re.search(r'[0-9]+',each_item['href']).group(0))
+        if fi_id not in already_item_id:#说明没爬过
+            browse_page = basement + each_item['href']
+            temp = sif.one_item(browse_page)
+            if temp.is_crawlable():
+                temp.info_total()
+                temp.info_characters()
+                temp.info_comments()
+                temp.info_persons()
+                raw_data.loc[raw_data.shape[0]] = temp.info#添加一行
             n += 1
             if (n%5 == 0):#每满五条输出一次
                 print('已扫描' + str(n) + '个产品，输出一次')#输出日志
+                print("当前时间： ",time.strftime('%Y.%m.%d %H:%M:%S ',time.localtime(time.time())))
                 raw_data.to_excel('raw_data.xlsx')#原始爬取数据输出
+        else:
+            print(str(fi_id) + '已爬过，故跳过')
     #判断是否为最后一页
     is_last_page = present_bs0bj.find('div',{'class':'page_inner'})
     if '››' in is_last_page.get_text():#说明还有下一页
